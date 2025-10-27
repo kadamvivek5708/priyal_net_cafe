705e1087efdf78f9966a7185b566ba4a019d666f
Vivek Kadam <kadamvivek5708@gmail.com>
2025-10-28 01:34:43 +0530

    initialize server setup with Express, MongoDB connection, and environment configuration

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+export const DB_NAME = "net_cafe";
---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+import {app} from "./app.js"
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+    .then(() => {
+        app.on("error", (error) => {
+            console.log("ERR0R: ", error);
+            throw error
+        })
+        app.listen(process.env.PORT || 8000 , () => {  
+            console.log("server is running at ", process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(`mongodb connection failed ${error}`)
+    })
